c63ef49b8811ac9218ea9e6962bf3b26461dc08e
Casey <r.casey.hilliard@gmail.com>
2015-09-08 15:46:43 -0300

    Initial population
    ONC parser (0_), based on ais.py from GPSD. Scripts 3a_, 3b_ require
    pyqgis, 3c_ requires pyshp, and is significantly slower.

---
+++ 1b_split_ONC_AIS_pre_tracks_NEMES.py
@@ -0,0 +1,185 @@
+#!/usr/bin/python
+# Split pre-parsed ONC-provided position-referenced AIS data (parsed using 
+# variation on ais.py from GPSD project) into basic movement data files on 
+# message type group.
+
+from glob import glob
+import sys
+import os
+import time
+
+# Function (ins_number) to determine if a string represents a number (specifically, an mmsi, code from: http://stackoverflow.com/questions/354038/how-do-i-check-if-a-string-is-a-number-float-in-python).
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+# End function (is_number)
+
+# Usage string for the script.
+usage_string = "Usage: split_ONC_AIS_pre_tracks_NEMES.py outputdirectory outputfilenameprefix inputfilename1 [inputfilename2 ...] \n\nSplits pre-parsed position-referenced AIS records, as provided by ONC, parsed by 0_gpsd_ais_ONC_Parsing.py into sub files by message type, while re-ordering the incoming data to match the format expected by the track division script, 2_divide_tracks_v_NEMES.py. Script created for NEMES project.\n"
+
+# If at least two arguments are not provided, display an usage message.
+if (len(sys.argv) < 4):
+    print usage_string
+    quit()
+    
+# retrieve the output directory and filename prefix
+outdirectory = sys.argv[1]
+out_filename_prefix = sys.argv[2]
+
+# Define an array of output filenames, based on the provided prefix, to store the parsed results.
+out_filename_array = [out_filename_prefix + "_1_2_3.csv", out_filename_prefix + "_5.csv", out_filename_prefix + "_18_19.csv", out_filename_prefix + "_other.csv"]
+
+# Check each potential output file for existence before running.
+for outfile_index in range(len(out_filename_array) - 1):
+    if os.path.exists(out_filename_array[outfile_index]):
+        print "Error, output file exists: (" + out_filename_array[outfile_index] +  ") aborting."
+        quit()
+        
+# Open all output files required.
+out_message_records = []
+
+for outfile_index in range(len(out_filename_array)):
+    try:
+        #out_message_records[outfile_index] = open(out_filename_array[outfile_index], 'w')
+        out_message_records.append(open(out_filename_array[outfile_index], 'w'))
+    except IOError:
+        print "Error opening output file: " + out_filename_array[outfile_index] + "\n"
+        quit()
+
+# Print a header line for each of the message type groups to be extracted from the eE AIS data.
+
+#1_2_3
+out_message_records[0].write("ext_timestamp,msgid,mmsi,nav_stat,sog,cog,tr_hdg,lat,lon,pos_acc\n");
+
+#5
+out_message_records[1].write("ext_timestamp,msgid,repeat,mmsi,ais_ver,imo,call_sign,vsl_name,ship_type,dim_bow,dim_ster,dim_port,dim_star,pos_fix,eta_min,eta_day,eta_hour,eta_min,draught,dest,dte\n"
+);
+
+#18_19 
+out_message_records[2].write("ext_timestamp,msgid,mmsi,nav_stat,sog,cog,tr_hdg,lat,lon,pos_acc\n");
+   
+#other
+out_message_records[3].write("Field set depends on message type, see 0_gpsd_ais_ONC_parsing.py \n")
+
+# Process each input file reference passed as input.
+for infile_index in range(len(sys.argv) - 3):
+
+    # Attempt wildcard expansion on any input file specified.
+    for in_filename in glob(sys.argv[(3 + infile_index)]):
+    
+        print("Processing: " + in_filename)
+        
+        with open(in_filename,'r') as in_vessel_records:
+        
+            # Reset a counter into the input file.
+            in_line_counter = 0
+        
+            for line in in_vessel_records:
+                
+                # Tokenize the record data on the basis of pipe-character.
+                pipetokenizedline = line.strip().split('|')
+                
+                # Copy the type value of the input message.
+                input_msg_type = pipetokenizedline[1]
+                
+                # Output tokenized fields according to the message type observed.
+                #1_2_3                
+                if(input_msg_type in ("1", "2", "3")):
+
+                    # Extract the date in "yyyymmddThhnnss.sssZ" format and convert to %Y%m%d_%H%M%S 
+                    # format, rounding the seconds value to an whole digit.
+                    in_date = pipetokenizedline[0]
+                    out_date = in_date[0:8] + "_" + in_date[9:13] + "%02d" % (int(round(float(in_date[13:19]))))
+
+                                
+                    # ext_timestamp,msgid,mmsi,nav_stat,sog,cog,tr_hdg,lat,lon,pos_acc
+                    # ONC Parsed: 0,1,3,4,6,10,11,9,8,7
+                    out_message_records[0].write(out_date + "," + pipetokenizedline[1] + "," + pipetokenizedline[3] + "," + pipetokenizedline[4] + "," + pipetokenizedline[6] + "," + pipetokenizedline[10] + "," + pipetokenizedline[11] + "," + pipetokenizedline[9] + "," + pipetokenizedline[8] + "," + pipetokenizedline[7] + "\n")
+
+                #5 - Write all tokens.
+                elif(input_msg_type in ("5")):
+
+                    # Extract the date in "yyyymmddThhnnss.sssZ" format and convert to %Y%m%d_%H%M%S 
+                    # format, rounding the seconds value to an whole digit.
+                    in_date = pipetokenizedline[0]
+                    out_date = in_date[0:8] + "_" + in_date[9:13] + "%02d" % (int(round(float(in_date[13:19]))))
+                
+                    out_message_records[1].write(out_date + "|" + "|".join(pipetokenizedline[1:]))
+
+                #18_19 
+                elif(input_msg_type in ("18", "19")):
+
+                    # Extract the date in "yyyymmddThhnnss.sssZ" format and convert to %Y%m%d_%H%M%S 
+                    # format, rounding the seconds value to an whole digit.
+                    in_date = pipetokenizedline[0]
+                    out_date = in_date[0:8] + "_" + in_date[9:13] + "%02d" % (int(round(float(in_date[13:19]))))
+
+                    #ext_timestamp,msgid,mmsi,nav_stat,sog,cog,tr_hdg,lat,lon,pos_acc
+                    # ONC Parsed:  0,1,3,N/A,5,9,10,8,7,6
+                    out_message_records[2].write(out_date + "," + pipetokenizedline[1] + "," + pipetokenizedline[3] + "," + "" + "," + pipetokenizedline[5] + "," 
+                    + pipetokenizedline[9] + "," + pipetokenizedline[10] + "," + pipetokenizedline[8] + "," + pipetokenizedline[7] + "," + pipetokenizedline[6] + "," + "\n")
+                    
+                #other - Write all tokens.
+                else:
+                
+                    # Extract the date in "yyyymmddThhnnss.sssZ" format and convert to %Y%m%d_%H%M%S 
+                    # format, rounding the seconds value to an whole digit.
+                    in_date = pipetokenizedline[0]
+                    out_date = in_date[0:8] + "_" + in_date[9:13] + "%02d" % (int(round(float(in_date[13:19]))))
+                
+                    out_message_records[3].write(out_date + "|" + "|".join(pipetokenizedline[1:]))
+
+                    
+                # Increment the current input line counter.
+                in_line_counter += 1
+                    
+# Close / flush all output files required.
+for outfile_index in range(len(out_message_records)):  
+    out_message_records[outfile_index].close()
+    
+# Run through the 1,2,3 and 18,19 parsed files, allocating the records within to 
+# new output files on the basis of mmsi.
+for data_index in {0,2}:
+    
+    print("Processing: " + out_filename_array[(data_index)])
+
+    with open(out_filename_array[data_index],'r') as in_parsed_sing_AIS_underway:
+
+        for line in in_parsed_sing_AIS_underway:
+            
+            tokenizedline = line.split(',')
+
+            # Verify that the incoming line has the correct number of tokens.
+            if(len(tokenizedline) > 8):
+                
+                mmsi = tokenizedline[2];
+                
+                # If the mmsi token is numeric, use its value as the output filename.
+                if is_number(mmsi):
+                
+                    try:
+                        outfile = open(outdirectory  + "/" + mmsi + ".txt", 'a')
+                    except IOError:
+                        print "Error opening file MMSI ->: " + outdirectory + mmsi + "\n"
+                        continue
+                        
+                    outfile.write(line)
+                    outfile.close()
+
+                        
+                # If the mmsi token is non-numeric, write the output to an "other" file.
+                else:
+                    try:
+                        outfile = open(outdirectory  + "/other.txt", 'a')
+                    except IOError:
+                        print "Error opening \"other\" output file.\n"
+                        continue
+                    
+                    outfile.write(line)
+                    outfile.close()
+            else:
+                
+                print("Error, incorrect number of tokens:" + str(len(tokenizedline)) + "Line:" + line + "\n")
---
+++ 3a_build_tracklines_and_points_NEMES.py
@@ -0,0 +1,360 @@
+#!/usr/bin/python
+
+# Import qgis core libraries
+from qgis.core import *
+from PyQt4.QtCore import *
+
+import sys
+from glob import glob
+
+# build_tracklines_NEMES.py - Converts a set of input files, as generated /
+# formatted by divide_tracks_v_NEMES.py, and generates a GIS layer (ESRI 
+# Shapefile) of track segments (as polyline objects), using the combination 
+# of MMSI number and track ID (added by divide_tracks_v_NEMES.py) as a 
+# primary key, and assuming the points are in chronological order. Requires
+# that QGIS be installed in the calling environment and be available
+# to python via pyqgis. Flags tracklines which contain point sequences 
+# with implied speeds that suggest there are bad points contained within.
+
+#########################################################
+from math import radians, cos, sin, asin, sqrt
+
+def haversine(lon1, lat1, lon2, lat2):
+
+    #Calculate the great circle distance between two points 
+    #on the earth (specified in decimal degrees)
+    
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+
+    # 6367 km is the radius of the Earth
+    km = 6367 * c
+    return km 
+#########################################################
+
+# Requires access to QGIS python resources, e.g:
+# export PYTHONPATH=/qgispath/share/qgis/python
+# export LD_LIBRARY_PATH=/qgispath/lib
+
+# supply path to where is your qgis installed
+QgsApplication.setPrefixPath("/usr/share/qgis", True)
+
+# load providers
+QgsApplication.initQgis()
+
+# If the wrong number of arguments is provided, display an usage message.
+if (len(sys.argv) < 3):
+    print 'Usage: build_tracklines_NEMES.py outputshapefileprefix inputfilename [inputfilename ...] ... \n Reads the specified input filenames, presumed to correspond to csv files of waypoint data, one per mmsi, ordered by timestamp. (output of divide_tracks_v_NEMES.py).\n'
+    quit()
+
+# Copy the output filename from the argument vector.
+out_line_filename = sys.argv[1] + "_lines.shp"
+out_point_filename = sys.argv[1] + "_points.shp"
+
+# Define the data fields to be included in the output track layer.
+trackdatafields = QgsFields()
+trackdatafields.append(QgsField("TrackID", QVariant.Int, "", 20))
+trackdatafields.append(QgsField("mmsi", QVariant.String, "", 12))
+trackdatafields.append(QgsField("elp_sec", QVariant.Int, "", 20))
+trackdatafields.append(QgsField("st_date", QVariant.String, "", 20))
+trackdatafields.append(QgsField("en_date", QVariant.String, "", 20))
+trackdatafields.append(QgsField("bad_spd", QVariant.Int, "", 20))
+
+# Define the data fields to be included in the output point layer.
+pointdatafields = QgsFields()
+pointdatafields.append(QgsField("TrackID", QVariant.Int, "", 20))
+pointdatafields.append(QgsField("msgid", QVariant.Int, "", 20))
+pointdatafields.append(QgsField("mmsi", QVariant.String, "", 12))
+pointdatafields.append(QgsField("navstatus", QVariant.String, "", 28)) ### This is to accomodate the gpsd interpreted text for nav status, might be better to truncate or re-enumerate for size.
+pointdatafields.append(QgsField("sog", QVariant.Double, "", 5, 1))
+pointdatafields.append(QgsField("cog", QVariant.Double, "", 5, 1))
+pointdatafields.append(QgsField("tr_hdg", QVariant.Double, "", 5, 1))
+pointdatafields.append(QgsField("pos_acc", QVariant.Int, "", 20))
+pointdatafields.append(QgsField("ais_date", QVariant.String, "", 20))
+pointdatafields.append(QgsField("hydro_dist", QVariant.Double, "", 12, 6))
+pointdatafields.append(QgsField("bad_spd", QVariant.Int, "", 20))
+
+# Hydrophone Coordinate
+hydro_lon = -135.3050
+hydro_lat = 53.3055
+
+# Instantiate the output file writer objects.
+trackwriter = QgsVectorFileWriter(out_line_filename,"CP1250",trackdatafields,QGis.WKBLineString,None,"ESRI Shapefile")
+pointwriter = QgsVectorFileWriter(out_point_filename,"CP1250",pointdatafields,QGis.WKBPoint,None,"ESRI Shapefile")
+
+
+# If an error occurs while creating the output file writer object, display the error and abort.
+if trackwriter.hasError() != QgsVectorFileWriter.NoError:
+    print "Error when creating shapefile: ", trackwriter.hasError()
+    quit()
+if pointwriter.hasError() != QgsVectorFileWriter.NoError:
+    print "Error when creating shapefile: ", pointwriter.hasError()
+    quit()
+    
+# Iterate over the input files specified, parsing each.
+for infile_index in range(len(sys.argv) - 2):
+    
+   
+    # Attempt wildcard expansion on any input file specified.
+    for in_filename in glob(sys.argv[(2 + infile_index)]):
+    
+        # DEBUG
+        print("Processing: " + in_filename)
+        # DEBUG
+        
+        with open(in_filename,'r') as in_track_records:
+        
+            # Initialize an index into the track ID values and a list to hold waypoints while tracks are constructed.
+            prev_inTrackID = -1
+            trackPoints = []
+            
+            # Iterate over the lines in the current file of track data.
+            for line in in_track_records:
+                
+                # Split the incoming csv line.
+                tokenizedline = line.split(',')
+                inTrackID = int(tokenizedline[0])
+
+                # If the current line is the first of the input file, initialize variables.
+                if(prev_inTrackID == -1):
+                    
+                    prev_inTrackID = inTrackID
+                    
+                    # Parse the values from the input line.
+                    # Adjusted field set: 20150817
+                    inMinSeconds = int(tokenizedline[1])
+                    inMaxSeconds = inMinSeconds
+                    inPrevSeconds = inMinSeconds
+                    inMinDate = tokenizedline[2]
+                    inMaxDate = inMinDate
+                    inMsgId = tokenizedline[3]
+                    inMMSI = tokenizedline[4]
+                    inNavStatus = tokenizedline[5]
+                    
+                    if ((tokenizedline[6] == "n/a") or (tokenizedline[6] == "None") or (tokenizedline[6] == "")):
+                        inSog = float(102.3)
+                    else:
+                        if tokenizedline[6] == "fast":
+                            inSog = float(102.2)
+                        else:
+                            inSog = float(tokenizedline[6])
+                    
+                    if ((tokenizedline[7] == "n/a") or (tokenizedline[7] == "None") or (tokenizedline[7] == "")):
+                        inCog = float(360.0)
+                    else:
+                        inCog = float(tokenizedline[7])
+                    
+                    if ((tokenizedline[8] == "n/a") or (tokenizedline[8] == "None") or (tokenizedline[8] == "")):
+                        inTrHdg = float(511.0)
+                    else:
+                        inTrHdg = float(tokenizedline[8])
+                        
+                    inLat = float(tokenizedline[9])
+                    inLon = float(tokenizedline[10])
+                    inPosAcc = tokenizedline[11]
+                    inPrevLat = inLat
+                    inPrevLon = inLon
+                    
+                    # Initialize a flag to store whether or not questionable implided speeds were encountered for a given track.
+                    badSpeedFlag = 0 
+                    
+                    # Append the current point to the list of points for the track.
+                    trackPoints.append(QgsPoint(inLon, inLat))
+                    
+                    ### (Begin) Add the current point to the point output layer
+                    # Calculate the distance from the point to the hydrophone.
+                    hydro_dist = haversine(inLon, inLat, hydro_lon, hydro_lat)
+                    
+                    fet = QgsFeature()
+                    fet.setGeometry(QgsGeometry.fromPoint(QgsPoint(inLon, inLat)))
+                    fet.initAttributes(11)
+                    fet.setAttribute(0, inTrackID)
+                    fet.setAttribute(1, inMsgId)
+                    fet.setAttribute(2, inMMSI)
+                    fet.setAttribute(3, inNavStatus)
+                    fet.setAttribute(4, inSog)
+                    fet.setAttribute(5, inCog)
+                    fet.setAttribute(6, inTrHdg)
+                    fet.setAttribute(7, inPosAcc)
+                    fet.setAttribute(8, inMaxDate) # Note: value in inMaxDate always valid for points.
+                    fet.setAttribute(9, hydro_dist)
+                    fet.setAttribute(10, badSpeedFlag)
+                    pointwriter.addFeature(fet)
+                     ### (End) Add the current point to the point output layer
+                    
+                # If the current line is the first of a new track, terminate the existing track 
+                # before initializing the next.
+                elif(prev_inTrackID <> inTrackID):
+                    
+                    # If the existing track has at least two points, proceed with writing it out.
+                    if(len(trackPoints) > 1):
+                    
+                        fet = QgsFeature()
+                        fet.setGeometry(QgsGeometry.fromPolyline(trackPoints))
+                        fet.initAttributes(6)
+                        fet.setAttribute(0, (prev_inTrackID))
+                        fet.setAttribute(1, (inMMSI))
+                        fet.setAttribute(2, (inMaxSeconds - inMinSeconds))
+                        fet.setAttribute(3, (inMinDate))
+                        fet.setAttribute(4, (inMaxDate))
+                        fet.setAttribute(5, (badSpeedFlag))
+                        trackwriter.addFeature(fet)
+                    
+                    prev_inTrackID = inTrackID
+                    
+                    # Parse the values from the input line.
+                    # Adjusted field set: 20150817
+                    inMinSeconds = int(tokenizedline[1])
+                    inMaxSeconds = inMinSeconds
+                    inPrevSeconds = inMinSeconds
+                    inMinDate = tokenizedline[2]
+                    inMaxDate = inMinDate
+                    inMsgId = tokenizedline[3]
+                    inMMSI = tokenizedline[4]
+                    inNavStatus = tokenizedline[5]
+                    
+                    if ((tokenizedline[6] == "n/a") or (tokenizedline[6] == "None") or (tokenizedline[6] == "")):
+                        inSog = float(102.3)
+                    else:
+                        if tokenizedline[6] == "fast":
+                            inSog = float(102.2)
+                        else:
+                            inSog = float(tokenizedline[6])
+                    
+                    if ((tokenizedline[7] == "n/a") or (tokenizedline[7] == "None") or (tokenizedline[7] == "")):
+                        inCog = float(360.0)
+                    else:
+                        inCog = float(tokenizedline[7])
+                    
+                    if ((tokenizedline[8] == "n/a") or (tokenizedline[8] == "None") or (tokenizedline[8] == "")):
+                        inTrHdg = float(511.0)
+                    else:
+                        inTrHdg = float(tokenizedline[8])
+                        
+                    inLat = float(tokenizedline[9])
+                    inLon = float(tokenizedline[10])
+                    inPosAcc = tokenizedline[11]
+                    inPrevLat = inLat
+                    inPrevLon = inLon
+                    
+                    # Reinitialize the flag to store whether or not questionable implided speeds were encountered for a given track.
+                    badSpeedFlag = 0 
+                    
+                    # Reset the existing list of points, then append the current point to the list of points for the next track.
+                    trackPoints = []
+                    trackPoints.append(QgsPoint(inLon, inLat))
+                    
+                    ### (Begin) Add the current point to the point output layer
+                    # Calculate the distance from the point to the hydrophone.
+                    hydro_dist = haversine(inLon, inLat, hydro_lon, hydro_lat)
+                    
+                    fet = QgsFeature()
+                    fet.setGeometry(QgsGeometry.fromPoint(QgsPoint(inLon, inLat)))
+                    fet.initAttributes(11)
+                    fet.setAttribute(0, inTrackID)
+                    fet.setAttribute(1, inMsgId)
+                    fet.setAttribute(2, inMMSI)
+                    fet.setAttribute(3, inNavStatus)
+                    fet.setAttribute(4, inSog)
+                    fet.setAttribute(5, inCog)
+                    fet.setAttribute(6, inTrHdg)
+                    fet.setAttribute(7, inPosAcc)
+                    fet.setAttribute(8, inMaxDate) # Note: value in inMaxDate always valid for points.
+                    fet.setAttribute(9, hydro_dist)
+                    fet.setAttribute(10, badSpeedFlag)
+                    pointwriter.addFeature(fet)
+                     ### (End) Add the current point to the point output layer
+                    
+                # If the current line extends the current track, aggregate the values as required.
+                else:
+                    
+                    # Copy the previous timestamp and position values.
+                    inPrevSeconds = inMaxSeconds
+                    inPrevLat = inLat
+                    inPrevLon = inLon
+                    
+                    # Parse the values from the input line.
+                    # Adjusted field set: 20150817
+                    inMaxSeconds = int(tokenizedline[1])
+                    inMaxDate = tokenizedline[2]
+                    inMsgId = tokenizedline[3]
+                    inMMSI = tokenizedline[4]
+                    inNavStatus = tokenizedline[5]
+                    
+                    if ((tokenizedline[6] == "n/a") or (tokenizedline[6] == "None") or (tokenizedline[6] == "")):
+                        inSog = float(102.3)
+                    else:
+                        if tokenizedline[6] == "fast":
+                            inSog = float(102.2)
+                        else:
+                            inSog = float(tokenizedline[6])
+                    
+                    if ((tokenizedline[7] == "n/a") or (tokenizedline[7] == "None") or (tokenizedline[7] == "")):
+                        inCog = float(360.0)
+                    else:
+                        inCog = float(tokenizedline[7])
+                    
+                    if ((tokenizedline[8] == "n/a") or (tokenizedline[8] == "None") or (tokenizedline[8] == "")):
+                        inTrHdg = float(511.0)
+                    else:
+                        inTrHdg = float(tokenizedline[8])
+                        
+                    inLat = float(tokenizedline[9])
+                    inLon = float(tokenizedline[10])
+                    inPosAcc = tokenizedline[11]
+                    
+                    # If the distance and time between the previous and current positions.
+                    timeDelta = inMaxSeconds - inPrevSeconds
+                    distanceDelta = haversine(inPrevLon, inPrevLat, inLon, inLat)
+                    if (timeDelta > 0) and ((distanceDelta / timeDelta) > 0.0444444444):
+                        
+                        badSpeedFlag = 1
+                        
+                    # Append the current point to the list of points for the track.
+                    trackPoints.append(QgsPoint(inLon, inLat))
+                    
+                    ### (Begin) Add the current point to the point output layer
+                    # Calculate the distance from the point to the hydrophone.
+                    hydro_dist = haversine(inLon, inLat, hydro_lon, hydro_lat)
+                    
+                    fet = QgsFeature()
+                    fet.setGeometry(QgsGeometry.fromPoint(QgsPoint(inLon, inLat)))
+                    fet.initAttributes(11)
+                    fet.setAttribute(0, inTrackID)
+                    fet.setAttribute(1, inMsgId)
+                    fet.setAttribute(2, inMMSI)
+                    fet.setAttribute(3, inNavStatus)
+                    fet.setAttribute(4, inSog)
+                    fet.setAttribute(5, inCog)
+                    fet.setAttribute(6, inTrHdg)
+                    fet.setAttribute(7, inPosAcc)
+                    fet.setAttribute(8, inMaxDate) # Note: value in inMaxDate always valid for points.
+                    fet.setAttribute(9, hydro_dist)
+                    fet.setAttribute(10, badSpeedFlag)
+                    pointwriter.addFeature(fet)
+                     ### (End) Add the current point to the point output layer
+
+            # If the last remaining track has at least two points, proceed with writing it out.
+            if(len(trackPoints) > 1):
+                
+                fet = QgsFeature()
+                fet.setGeometry(QgsGeometry.fromPolyline(trackPoints))
+                
+                fet.initAttributes(6)
+                fet.setAttribute(0, (prev_inTrackID))
+                fet.setAttribute(1, (inMMSI))
+                fet.setAttribute(2, (inMaxSeconds - inMinSeconds))
+                fet.setAttribute(3, (inMinDate))
+                fet.setAttribute(4, (inMaxDate))
+                fet.setAttribute(5, (badSpeedFlag))
+                trackwriter.addFeature(fet)
+
+# Delete the writers to flush features to disk.
+del trackwriter
+del pointwriter
