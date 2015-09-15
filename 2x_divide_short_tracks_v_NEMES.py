db9d4a5d6319929d392b846b58c1a313bac2eddd
Casey <r.casey.hilliard@gmail.com>
2015-09-15 14:51:51 -0300

    Update for point-to-point processing
    Updated 2x script to use glob for wildcard expansion under Windows.
    Fixed path separator -> adjusted to auto detect

--- 2x_divide_short_tracks_v_NEMES.py
+++ 2x_divide_short_tracks_v_NEMES.py
@@ -26,8 +26,10 @@
 # only segment pairs, and which also calculates and stores directions
 # with each segment.
 
+from glob import glob
 import sys
 import time
+from os import sep
 
 #########################################################
 from math import radians, cos, sin, asin, sqrt, atan2, pi
@@ -75,6 +77,17 @@ def bearing(lon1, lat1, lon2, lat2):
     
 #########################################################
 
+# Establish a path separator. 
+# CH - Ugly, detect this rather than set 
+# Linux
+#path_separator = "/"
+# Windows
+#path_separator = "\\"
+path_separator = sep
+
+#DEBUG
+print path_separator
+
 if (len(sys.argv) < 3):
     print 'Usage: divide_short_tracks_v_NEMES.py outputdirectory inputfilename [inputfilename ...] ... \n Adds track delineation to a number of input files of vessel underway data, predivided into single files per mmsi under the specified output directory.\n'
     quit()
@@ -87,133 +100,138 @@ track_separation_time = 180
 
 for infile_index in range(len(sys.argv) - 2):
     
-    print("Processing: " + sys.argv[(2 + infile_index)])
+    # Attempt wildcard expansion on any input file specified (Only (likely) req'd for Windows).
+    for in_filename in glob(sys.argv[(2 + infile_index)]):
+    
+        print("Processing: " + in_filename)
 
-    (dummyone, dummytwo, outfilename) = sys.argv[2 + infile_index].rpartition("/")
+        (dummyone, dummytwo, outfilename) = in_filename.rpartition(path_separator)
 
-    try:
-        out_vessel_records = open(outdirectory + "/" + outfilename, 'w')
-    except IOError:
-        print "Error opening file: " + outdirectory + "/" + outfilename + "\n"
-        quit()
+        try:
+            out_vessel_records = open(outdirectory + path_separator + outfilename, 'w')
+        except IOError:
+            print "Error opening file: " + outdirectory + path_separator + outfilename + "\n"
+            quit()
 
-    try:
-        out_stationary_records = open(outdirectory + "/stationary_" + outfilename, 'w')
-    except IOError:
-        print "Error opening file: " + outdirectory + "/stationary_" + outfilename + "\n"
-        quit()
-        
-    try:
-        out_invalid_records = open(outdirectory + "/invalid_" + outfilename, 'w')
-    except IOError:
-        print "Error opening file: " + outdirectory + "/invalid_" + outfilename + "\n"
-        quit()
-
-    with open(sys.argv[2 + infile_index],'r') as in_vessel_records:
-        
-        # Initialize defaults.
-        track_index = 0
-        prev_timeval = 0
-        prev_latitude = -999
-        prev_longitude = -999
-        
-        track_point_counter = 0
-        
-        # Tokenize each line of input from the source file, presuming comma delimiters.
-        for line in in_vessel_records:
+        try:
+            out_stationary_records = open(outdirectory + path_separator + "stationary_" + outfilename, 'w')
+        except IOError:
+            print "Error opening file: " + outdirectory + path_separator + "stationary_" + outfilename + "\n"
+            quit()
             
-            tokenizedline = line.strip().split(',')
-            datetimetoken = tokenizedline[0]
-            mmsi = tokenizedline[3]
-            navstatus = tokenizedline[4]
-            latitude = tokenizedline[8]
-            longitude = tokenizedline[9]
-            shiptype = tokenizedline[11]
-            shipname = tokenizedline[12]
-            
-            timevalstruct = time.strptime(datetimetoken, "%Y%m%d_%H%M%S")
-            timeval = time.mktime(timevalstruct)
+        try:
+            out_invalid_records = open(outdirectory + path_separator + "invalid_" + outfilename, 'w')
+        except IOError:
+            print "Error opening file: " + outdirectory + path_separator + "invalid_" + outfilename + "\n"
+            quit()
 
-            # If the vessel's position is outside the reasonable range (e.g. 91 indicating unavailable within AIS), 
-            # drop the position into the file for invalid records.
-            if ((abs(float(latitude)) >= 90)):
-                out_invalid_records.write(line)
-                    
-            # If the vessel's position has not changed between records, update the stored values, 
-            # but do not write the position out. Insert a copy of the line into the stationary
-            # records file.            (Note: Could / should be replaced with 
-            # subroutine to seek and drop all strings of stationary positions as discrete 
-            # stationary objects).
-            elif ((latitude == prev_latitude) and (longitude == prev_longitude)) or (prev_timeval == timeval):
-
-                prev_datetimetoken = datetimetoken
-                prev_mmsi = mmsi
-                prev_navstatus = navstatus
-                prev_latitude = latitude
-                prev_longitude = longitude
-                prev_shiptype = shiptype
-                prev_shipname = shipname
-                prev_timeval = timeval
+        with open(in_filename,'r') as in_vessel_records:
             
-                out_stationary_records.write(line)
+            # Initialize defaults.
+            track_index = 0
+            prev_timeval = 0
+            prev_latitude = -999
+            prev_longitude = -999
+            
+            track_point_counter = 0
+            
+            # Tokenize each line of input from the source file, presuming comma delimiters.
+            for line in in_vessel_records:
                 
-            else:
+                tokenizedline = line.split(',')
+                datetimetoken = tokenizedline[0]
+                msgid = tokenizedline[1]
+                mmsi = tokenizedline[2]
+                navstatus = tokenizedline[3]
+                sog = tokenizedline[4]
+                cog = tokenizedline[5]
+                tr_hdg = tokenizedline[6]
+                latitude = tokenizedline[7]
+                longitude = tokenizedline[8]
+                pos_acc = tokenizedline[9]
                 
-                # If the current point is the first of a track segment, store the value 
-                # and wait until further points accrue before writing out values.
-                if(track_point_counter == 0):
-                    
-                    # Increment the track point counter.
-                    track_point_counter = 1
-                    
-                    # Store the current values as previous ("-1").
-                    prev_datetimetoken = datetimetoken
-                    prev_mmsi = mmsi
-                    prev_navstatus = navstatus
-                    prev_latitude = latitude
-                    prev_longitude = longitude
-                    prev_shiptype = shiptype
-                    prev_shipname = shipname
-                    prev_timeval = timeval
+                timevalstruct = time.strptime(datetimetoken, "%Y%m%d_%H%M%S")
+                timeval = time.mktime(timevalstruct)
+
+                # If the vessel's position is outside the reasonable range (e.g. 91 indicating unavailable within eE AIS, or n/a 
+                # from AIS parsed using GPSD parser.), 
+                # drop the position into the file for invalid records.
+                if latitude == 'n/a' or ((abs(float(latitude)) >= 90)):
+                    out_invalid_records.write(line)
+                        
+                # If the vessel's position has not changed between records, drop the position 
+                # into the file for stationary records (Note: Could / should be replaced with 
+                # subroutine to seek and drop all strings of stationary positions as discrete 
+                # stationary objects).
+                elif ((latitude == prev_latitude) and (longitude == prev_longitude)) or (prev_timeval == timeval):
                 
-                # If the current point is the second of a track segment, calculate the 
-                # speed and direction, and write out the pair to the output.
-                elif(track_point_counter == 1):
+                    out_stationary_records.write(line)
                     
-                    # Calculate the elapsed time between the points.
-                    elapsed_segment_time = timeval - prev_timeval
+                else:
                     
-                    # Calculate the distance between the positions.
-                    segment_length = haversine(float(prev_longitude), float(prev_latitude), float(longitude), float(latitude))
+                    # If the current point is the first of a track segment, store the value 
+                    # and wait until further points accrue before writing out values.
+                    if(track_point_counter == 0):
+                        
+                        # Increment the track point counter.
+                        track_point_counter = 1
+                        
+                        # Store the current values as previous ("-1").
+                        prev_datetimetoken = datetimetoken
+                        prev_msgid = msgid
+                        prev_mmsi = mmsi
+                        prev_navstatus = navstatus
+                        prev_sog = sog
+                        prev_cog = cog
+                        prev_tr_hdg = tr_hdg
+                        prev_latitude = latitude
+                        prev_longitude = longitude
+                        prev_pos_acc = pos_acc
+                        prev_timeval = timeval
                     
-                    # Calculate the bearing between the positions.
-                    segment_bearing = bearing(float(prev_longitude), float(prev_latitude), float(longitude), float(latitude))
-                    
-                    # CCCC - Removed navstatus from output, was unused; could revisit later.
-                    
-                    # Print out the first point of the pair.
-                    out_vessel_records.write("" + str(track_index) + "," + datetimetoken + "," + mmsi + "," + str(latitude) + "," + str(longitude) + "," + shiptype + "," + shipname + "," + str(elapsed_segment_time) + "," + str(segment_length) + "," + str(segment_bearing) + "\n")
-                    
-                    # Print out the second point of the pair
-                    out_vessel_records.write("" + str(track_index) + "," + prev_datetimetoken + "," + prev_mmsi + "," + str(prev_latitude) + "," + str(prev_longitude) + "," + prev_shiptype + "," + prev_shipname + "," + str(elapsed_segment_time) + "," + str(segment_length) + "," + str(segment_bearing) + "\n")
-                    
-                    # Leave the track point counter at 1.
-                    track_point_counter = 1
-                    
-                    # Increment the track index.
-                    track_index += 1
-                    
-                    # Store the current values as previous.
-                    prev_datetimetoken = datetimetoken
-                    prev_mmsi = mmsi
-                    prev_navstatus = navstatus
-                    prev_latitude = latitude
-                    prev_longitude = longitude
-                    prev_shiptype = shiptype
-                    prev_shipname = shipname
-                    prev_timeval = timeval
- 
-    # Close the output files.
-    out_vessel_records.close
-    out_stationary_records.close
-    out_invalid_records.close
+                    # If the current point is the second of a track segment, calculate the 
+                    # speed and direction, and write out the pair to the output.
+                    elif(track_point_counter == 1):
+                        
+                        # Calculate the elapsed time between the points.
+                        elapsed_segment_time = timeval - prev_timeval
+                        
+                        # Calculate the distance between the positions.
+                        segment_length = haversine(float(prev_longitude), float(prev_latitude), float(longitude), float(latitude))
+                        
+                        # Calculate the bearing between the positions.
+                        segment_bearing = bearing(float(prev_longitude), float(prev_latitude), float(longitude), float(latitude))
+                        
+                        # CCCC - Removed navstatus from output, was unused; could revisit later.
+                        
+                        # Print out the first point of the pair.
+                        # old 20150915 out_vessel_records.write("" + str(track_index) + "," + datetimetoken + "," + mmsi + "," + str(latitude) + "," + str(longitude) + "," + shiptype + "," + shipname + "," + str(elapsed_segment_time) + "," + str(segment_length) + "," + str(segment_bearing))
+                        out_vessel_records.write("" + str(track_index) + "," + str(int(prev_timeval)) + "," + prev_datetimetoken + "," + prev_msgid + "," + prev_mmsi + "," + prev_navstatus + "," + prev_sog + "," + prev_cog + "," + prev_tr_hdg + "," + str(latitude) + "," + str(longitude) + "," + prev_pos_acc)
+                        
+                        # Print out the second point of the pair
+                        # old 20150915 out_vessel_records.write("" + str(track_index) + "," + prev_datetimetoken + "," + prev_mmsi + "," + str(prev_latitude) + "," + str(prev_longitude) + "," + prev_shiptype + "," + prev_shipname + "," + str(elapsed_segment_time) + "," + str(segment_length) + "," + str(segment_bearing))                        
+                        out_vessel_records.write("" + str(track_index) + "," + str(int(timeval)) + "," + datetimetoken + "," + msgid + "," + mmsi + "," + navstatus + "," + sog + "," + cog + "," + tr_hdg + "," + str(latitude) + "," + str(longitude) + "," + pos_acc)
+
+                        # Leave the track point counter at 1.
+                        track_point_counter = 1
+                        
+                        # Increment the track index.
+                        track_index += 1
+                        
+                        # Store the current values as previous.
+                        prev_datetimetoken = datetimetoken
+                        prev_msgid = msgid
+                        prev_mmsi = mmsi
+                        prev_navstatus = navstatus
+                        prev_sog = sog
+                        prev_cog = cog
+                        prev_tr_hdg = tr_hdg
+                        prev_latitude = latitude
+                        prev_longitude = longitude
+                        prev_pos_acc = pos_acc
+                        prev_timeval = timeval
+     
+        # Close the output files.
+        out_vessel_records.close
+        out_stationary_records.close
+        out_invalid_records.close
